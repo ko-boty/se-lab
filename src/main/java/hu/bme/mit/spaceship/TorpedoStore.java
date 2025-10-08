0818d73cd86cc6ffb365725ebe1463e1078f1140
Kováts Botond <kovatsboti@gmail.com>
2025-10-08 17:14:26 +0200

    fixed '=-' operator on line 43

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -40,7 +40,7 @@ public class TorpedoStore {
     double r = this.generator.nextDouble();
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
